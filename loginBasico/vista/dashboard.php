f08ff9ec6478cc4f33ee97b7cc26fcba02857693
Carlos Jiménez <caliche18a@gmail.com>
2023-02-19 18:13:19 -0500

    creación de arquitectura MCV

---
+++ loginBasico/vista/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_POST['entrar'])) {
+    echo "Puede ingresar presionando el botón ingresar";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+<a href="newuser.html" target="_blank" rel="noopener noreferrer">Crear usuario</a>
+</body>
+</html>
